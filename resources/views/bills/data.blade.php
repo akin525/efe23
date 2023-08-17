ecbcf771015052db2e0239dd026661d03e746db8
ACCOUNTS <akinlabisamson15@gmail.com>
2023-08-17 09:10:46 +0100

    dish

--- resources/views/bills/data.blade.php
+++ resources/views/bills/data.blade.php
@@ -49,14 +49,13 @@
                                 <div class="card overflow-hidden">
                                     <div class="row">
                                             <div class="dropdown bootstrap-select default-select form-control wide mb-3 form-control-lg">
-                                                <br>
-                                                <select name="productid" class="form-control" id="secondSelect" >
+                                                <select name="productid" class="default-select form-control " id="secondSelect" >
                                                     <option>Select Your Plan</option>
                                                 @foreach($netm as $mtn)
                                                     <option value={{$mtn['id']}}"">{{$mtn['name']}} ₦{{$mtn['tamount']}}</option>
                                                     @endforeach
                                                 </select>
-                                                <br>
+
                                             </div>
                                     </div>
                                 </div>
@@ -85,15 +84,13 @@
                                 <div class="card overflow-hidden">
                                     <div class="row">
                                             <div class="dropdown bootstrap-select default-select form-control wide mb-3 form-control-lg">
-                                                <hr/>
-                                                <select name="productid" class="form-control" id="secondSelect" >
-                                                <option>Select Your Plan</option>
+                                                <select name="productid" class="default-select form-control wide mb-3 form-control-lg" id="secondSelect" tabindex="null">
+                                                    <option>Select Your Plan</option>
                                                 @foreach($neta as $air)
                                                     <option value={{$air['id']}}"">{{$air['network']}} {{$air['plan']}} ₦{{$air['tamount']}}</option>
                                                     @endforeach
                                                 </select>
 
-                                                <hr/>
                                             </div>
                                     </div>
                                 </div>
@@ -163,14 +160,12 @@
                                 <div class="card overflow-hidden">
                                     <div class="row">
                                             <div class="dropdown bootstrap-select default-select form-control wide mb-3 form-control-lg">
-                                                <hr/>
-                                                <select name="productid" class="form-control wide mb-3 form-control-lg" id="secondSelect" tabindex="null">
+                                                <select name="productid" class="default-select form-control wide mb-3 form-control-lg" id="secondSelect" tabindex="null">
                                                     <option>Select Your Plan</option>
                                                 @foreach($net9 as $air3)
                                                     <option value={{$air3['id']}}"">{{$air3['network']}} {{$air3['plan']}} ₦{{$air3['tamount']}}</option>
                                                     @endforeach
                                                 </select>
-                                                <hr/>
 
                                             </div>
                                     </div>
