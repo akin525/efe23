4809c2b1cda682bd4de5841d171171efacdf4eca
ACCOUNTS <akinlabisamson15@gmail.com>
2023-08-22 13:54:45 +0100

    dish

--- resources/views/task/ads.blade.php
+++ resources/views/task/ads.blade.php
@@ -98,7 +98,18 @@
                 <span class="input-group-text">Telephone</span>
                 <input class="form-control" value="{{Auth::user()->phone}}" type="number" name="number" >
             </div>
+        <style>
+            #imageContainer {
+                margin-top: 20px;
+            }
 
+            #displayedImage {
+                max-width: 100%;
+                height: auto;
+                margin-top: 10px;
+                display: none;
+            }
+        </style>
             <div class="card-header">
                 <h4 class="card-title">Addition Image</h4>
             </div>
@@ -106,7 +117,10 @@
                 <div class="basic-form">
                     <div class="dropzone">
                         <div class="dlab-default dlab-message">
-                            <input class="dlab-button" name="add" id="fileInput2" type="file" required/>
+                            <input class="dlab-button" name="add" id="imageInput" type="file" required/>
+                            <div class="card card-body" id="imageContainer">
+                                <img id="displayedImage" alt="Displayed Image">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -144,7 +158,10 @@
                                 <div class="dropzone dlab-clickable">
 
                                     <div class="dlab-default dlab-message">
-                                        <input class="dlab-button" name="cover"  type="file" required/>
+                                        <input class="dlab-button" name="cover" id="imageInput1"  type="file" required/>
+                                        <div class="card card-body" id="imageContainer1">
+                                            <img id="displayedImage1" alt="">
+                                        </div>
                                     </div>
                                 </div>
                                 </div>
@@ -159,6 +176,40 @@
     </div>
 @endsection
 @section('script')
+    <script>
+        const imageInput = document.getElementById("imageInput");
+        const displayedImage = document.getElementById("displayedImage");
+
+        imageInput.addEventListener("change", () => {
+            const file = imageInput.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    displayedImage.src = e.target.result;
+                    displayedImage.style.display = "block";
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+
+    </script>
+    <script>
+        const imageInput1= document.getElementById("imageInput1");
+        const displayedImage1 = document.getElementById("displayedImage1");
+
+        imageInput1.addEventListener("change", () => {
+            const file = imageInput1.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    displayedImage1.src = e.target.result;
+                    displayedImage1.style.display = "block";
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+
+    </script>
     <script src="{{asset('dashboard/vendor/dropzone/dist/dropzone.js')}}"></script>
     <script src="{{asset('dashboard/vendor/global/global.min.js')}}"></script>
     <script src="{{asset('dashboard/vendor/jquery-nice-select/js/jquery.nice-select.min.js')}}"></script>
